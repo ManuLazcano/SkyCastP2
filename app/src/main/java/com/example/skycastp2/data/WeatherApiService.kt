71ad0f2d661a760e6bd07d961e6b27a1944aa6c1
ManuLazcano <manu.lazcano17@gmail.com>
2024-11-18 18:31:51 -0300

    feat: add WeatherApiService structure for endpoints

---
+++ app/src/main/java/com/example/skycastp2/data/WeatherApiService.kt
@@ -0,0 +1,22 @@
+package com.example.skycastp2.data
+
+import com.example.skycastp2.BuildConfig
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface WeatherApiService {
+    @GET("current")
+    suspend fun getCurrentWeather(
+        @Query("city") city: String,
+        @Query("key") apiKey: String = BuildConfig.API_KEY,
+        @Query("lang") lang: String = "es"
+    ): CurrentWeatherResponse
+
+    @GET("forecast/daily")
+    suspend fun getForecast(
+        @Query("city") city: String,
+        @Query("days") days: Int = 5,
+        @Query("key") apiKey: String = BuildConfig.API_KEY,
+        @Query("lang") lang: String = "es"
+    ): ForecastResponse
+}
