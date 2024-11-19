b818f37cff640e0864cbfbd9c19d542bed954669
ManuLazcano <manu.lazcano17@gmail.com>
2024-11-19 15:23:25 -0300

    refactor: add binding configuration for second activity

--- app/src/main/java/com/example/skycastp2/ForecastActivity.kt
+++ app/src/main/java/com/example/skycastp2/ForecastActivity.kt
@@ -5,16 +5,15 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.skycastp2.databinding.ActivityForecastBinding
 
 class ForecastActivity : AppCompatActivity() {
+    private lateinit var  binding: ActivityForecastBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_forecast)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+        binding = ActivityForecastBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
     }
 }
