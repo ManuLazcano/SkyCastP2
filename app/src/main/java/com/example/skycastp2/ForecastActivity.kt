842a1ceed2b1376cd267073c8ab8fcfd4c50d4e4
ManuLazcano <manu.lazcano17@gmail.com>
2024-11-21 02:01:58 -0300

    feat: using the database location on the second screen

--- app/src/main/java/com/example/skycastp2/ForecastActivity.kt
+++ app/src/main/java/com/example/skycastp2/ForecastActivity.kt
@@ -6,6 +6,9 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.skycastp2.adapter.ForecastAdapter
 import com.example.skycastp2.data.ForecastData
+import com.example.skycastp2.database.CityRepository
+import com.example.skycastp2.database.CityViewModel
+import com.example.skycastp2.database.DatabaseInstance
 import com.example.skycastp2.databinding.ActivityForecastBinding
 import com.example.skycastp2.viewModel.ForecastViewModel
 
@@ -13,16 +16,25 @@ class ForecastActivity : AppCompatActivity() {
     private lateinit var  binding: ActivityForecastBinding
     private lateinit var forecastAdapter: ForecastAdapter
     private val forecastViewModel: ForecastViewModel by viewModels()
+    private lateinit var cityViewModel: CityViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityForecastBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Llamada para cargar el pronóstico
-        forecastViewModel.fetchForecast("Buenos Aires")
+        // Obtener el Dao y Repository para Room
+        val cityDao = DatabaseInstance.getDatabase(applicationContext).cityDao()
+        val repository = CityRepository(cityDao)
+        cityViewModel = CityViewModel(repository)
 
-        // Observador de los datos
+        // Observar la ciudad guardada
+        cityViewModel.city.observe(this) { city ->
+            val cityName = city?.name ?: "Ushuaia"
+
+            forecastViewModel.fetchForecast(cityName)
+        }
+
         forecastViewModel.forecast.observe(this) { forecastResponse ->
             forecastResponse?.let {
                 initRecycler(it)
